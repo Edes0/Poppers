ca52570455c1bd3be242d578f7fe8d83ee692c2e
linneabooth <github@linneabooth.com>
2025-01-25 19:43:55 +0100

    Pause menu and buttons
    added pause menu and button to go to main menu and resume, need to add code for the pause menu to open as well though

--- Poppers/Assets/Scripts/UIManager.cs
+++ Poppers/Assets/Scripts/UIManager.cs
@@ -6,9 +6,10 @@ using UnityEngine.SceneManagement;
 public class UIManager : MonoBehaviour
 {
     public GameObject CanvasCredits;
+    public GameObject CanvasPause;
 
 
-
+    //Main menu//
     public void PlayGame()
     {
         SceneManager.LoadScene("InGameScene");
@@ -17,13 +18,13 @@ public class UIManager : MonoBehaviour
     public void ShowCredits()
     {
         CanvasCredits.gameObject.SetActive(true);
-        //gameObject GetComponent <Canvas>("CanvasCredits").enabled = true;
+        
     }
 
     public void CloseCredits()
     {
         CanvasCredits.gameObject.SetActive(false);
-        //gameObject GetComponent <Canvas>("CanvasCredits").enabled = true;
+        
     }
 
     public void doExitGame()
@@ -31,7 +32,23 @@ public class UIManager : MonoBehaviour
         Application.Quit();
     }
 
+    //Pause menu//
+    public void PauseGame()
+    {
+        CanvasPause.gameObject.SetActive(true);
+        //add code to hit esc for pause
+    }
 
+    public void UnpauseGame()
+    {
+        CanvasPause.gameObject.SetActive(false);
+
+    }
+
+    public void ExitGameplay()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
 
 
 }

5c323bc2213a4ead39b1c82892727bc0520e03cd
linneabooth <github@linneabooth.com>
2025-01-25 15:22:15 +0100

    UI Main menu
    Menu canvas and play button implemented

---
+++ Poppers/Assets/Scripts/UIManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class UIManager : MonoBehaviour
+{
+    
+    
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("InGameScene");
+    }
+
+
+
+}

08952e51a8691a89119e43c94d7201322168ecc2
linneabooth <github@linneabooth.com>
2025-01-25 18:08:08 +0100

    Implement credits
    Implemented credits popup with a close button

--- Poppers/Assets/Scripts/UIManager.cs
+++ Poppers/Assets/Scripts/UIManager.cs
@@ -1,15 +1,31 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 
 public class UIManager : MonoBehaviour
 {
-    
-    
+    public GameObject CanvasCredits;
+
+
+
     public void PlayGame()
     {
         SceneManager.LoadScene("InGameScene");
     }
+
+    public void ShowCredits()
+    {
+        CanvasCredits.gameObject.SetActive(true);
+        //gameObject GetComponent <Canvas>("CanvasCredits").enabled = true;
+    }
+
+    public void CloseCredits()
+    {
+        CanvasCredits.gameObject.SetActive(false);
+        //gameObject GetComponent <Canvas>("CanvasCredits").enabled = true;
+    }
+
 
 
 
